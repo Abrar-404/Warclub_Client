13fb6cdebb7402fa931b17157e699dd3cc12108c
Sahil Abrar <sahilabrar444@gmail.com>
2024-02-27 23:09:38 +0600

    adding bg image

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,7 +1,9 @@
 const Banner = () => {
   return (
-    <div>
-      
+    <div className="bg-[url('https://wordpress.themeholy.com/bame/wp-content/uploads/2023/12/hero-bg1-1.png')] h-screen bg-no-repeat">
+      <div>
+        banner
+      </div>
     </div>
   );
 };
