739e6c51be6d75fe0f6a8419e9a16b0182386e77
Sahil Abrar <sahilabrar444@gmail.com>
2024-02-27 23:10:30 +0600

    added padding

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 const Banner = () => {
   return (
     <div className="bg-[url('https://wordpress.themeholy.com/bame/wp-content/uploads/2023/12/hero-bg1-1.png')] h-screen bg-no-repeat">
-      <div>
+      <div className="pt-48">
         banner
       </div>
     </div>
