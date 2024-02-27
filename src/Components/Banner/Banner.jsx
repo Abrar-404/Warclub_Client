293ae39851de6d6c4711d38ddd7d3285ff3e15c6
Sahil Abrar <sahilabrar444@gmail.com>
2024-02-27 23:23:33 +0600

    added font classname

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,10 +1,14 @@
+import '../Styles/customFont.css'
+
 const Banner = () => {
   return (
     // <div className="bg-[url('https://wordpress.themeholy.com/bame/wp-content/uploads/2023/12/hero-bg1-1.png')] opacity-5 h-screen bg-no-repeat">
     //   </div>
     <div>
       <div className="pt-48">
-        <h1 className="text-[#45F882] text-2xl"># World Class ESports & Gaming Site</h1>
+        <h1 className="text-[#45F882] text-2xl rajdhani-medium">
+          # World Class ESports & Gaming Site
+        </h1>
       </div>
     </div>
   );
