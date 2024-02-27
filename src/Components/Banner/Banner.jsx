247fedfd8271af10e50df5aeed62bd1811c277aa
Sahil Abrar <sahilabrar444@gmail.com>
2024-02-27 23:14:20 +0600

    added bg backdrop

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 const Banner = () => {
   return (
-    <div className="bg-[url('https://wordpress.themeholy.com/bame/wp-content/uploads/2023/12/hero-bg1-1.png')] h-screen bg-no-repeat">
+    <div className="bg-[url('https://wordpress.themeholy.com/bame/wp-content/uploads/2023/12/hero-bg1-1.png')] opacity-5 h-screen bg-no-repeat">
       <div className="pt-48">
         banner
       </div>
