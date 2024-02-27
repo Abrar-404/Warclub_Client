400d09b77c3d83b0d4a84912f7ea13c89b20e08b
Sahil Abrar <sahilabrar444@gmail.com>
2024-02-27 12:29:13 +0600

    added navbar temp

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,79 @@
+const Navbar = () => {
+  return (
+    <div>
+      import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-container">
+        <a href="#" className="navbar-logo">
+          Bame Esports
+        </a>
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <a href="#" className="nav-links">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-links">
+              About Us
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-links">
+              Tournament
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-links">
+              Pages
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-links">
+              Blog
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-links">
+              Contact
+            </a>
+          </li>
+        </ul>
+        <ul className="nav-menu-right">
+          <li className="nav-item">
+            <a href="#" className="nav-links">
+              <i className="fab fa-facebook"></i>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-links">
+              <i className="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-links">
+              <i className="fab fa-pinterest"></i>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-links">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
+
+    </div>
+  );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,18 @@ import App from './App.jsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MainLayout from './Components/Layout/MainLayout.jsx';
+import Home from './Components/Pages/Home/Home.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
   },
 ]);
 
