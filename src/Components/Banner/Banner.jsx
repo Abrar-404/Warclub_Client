08d55785e544e2cc5975c7943f0fffc7301ac7e2
Sahil Abrar <sahilabrar444@gmail.com>
2024-02-27 23:17:12 +0600

    added banner heading

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,8 +1,10 @@
 const Banner = () => {
   return (
-    <div className="bg-[url('https://wordpress.themeholy.com/bame/wp-content/uploads/2023/12/hero-bg1-1.png')] opacity-5 h-screen bg-no-repeat">
+    // <div className="bg-[url('https://wordpress.themeholy.com/bame/wp-content/uploads/2023/12/hero-bg1-1.png')] opacity-5 h-screen bg-no-repeat">
+    //   </div>
+    <div>
       <div className="pt-48">
-        banner
+        <h1 className=""># World Class ESports & Gaming Site</h1>
       </div>
     </div>
   );
