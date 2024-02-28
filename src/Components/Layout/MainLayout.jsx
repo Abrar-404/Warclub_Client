c71abf8ed6a28670394bf363c94bbcbab8369a29
Sahil Abrar <sahilabrar444@gmail.com>
2024-02-28 21:16:25 +0600

    added animated cursor pack

--- src/Components/Layout/MainLayout.jsx
+++ src/Components/Layout/MainLayout.jsx
@@ -1,10 +1,26 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 import '../Styles/cursor.css';
+import AnimatedCursor from 'react-animated-cursor';
 
 const MainLayout = () => {
   return (
     <div className="h-auto bg-[url('https://themedox.com/demo/mykd/assets/img/bg/area_bg02.jpg')] svg mx-auto">
+      <AnimatedCursor
+        innerSize={10}
+        outerSize={50}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        trailingSpeed={4}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: 'white',
+        }}
+        outerStyle={{
+          border: '3px solid white',
+        }}
+      />
       <div>
         <Navbar></Navbar>
         <Outlet></Outlet>
