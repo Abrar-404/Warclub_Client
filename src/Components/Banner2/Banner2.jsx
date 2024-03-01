5f0709ab74dbedd6a773e23c5367b7e8f1a6c876
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-01 21:49:08 +0600

    added aos

--- src/Components/Banner2/Banner2.jsx
+++ src/Components/Banner2/Banner2.jsx
@@ -1,13 +1,16 @@
 import shape3 from '../../assets/shape3.png';
 import hero1 from '../../assets/hero1.png';
 import hero2 from '../../assets/hero2.png';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
 
 const Banner2 = () => {
   return (
-    <div>
+    <div className="flex justify-center">
       <div className="relative">
         <img src={shape3} alt="" />
-        <div className=" lg:w-2/4 md:w-2/3 w-[57%] absolute lg:top-[100px] md:top-[40px] lg:left-[329px] md:left-[120px] top-[42px] left-[90px]">
+        <div className=" lg:w-[57%] md:w-2/3 w-[57%] absolute lg:top-[100px] md:top-[40px] lg:left-[329px] md:left-[120px] top-[42px] left-[90px]">
           <div className="flex items-center justify-between">
             <img
               data-aos="fade-right"
@@ -29,3 +32,5 @@ const Banner2 = () => {
 };
 
 export default Banner2;
+
+AOS.init();
