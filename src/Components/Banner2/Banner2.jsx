afc8b346df3eab4b0c3a73e40a48a523008f5392
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-01 21:35:13 +0600

    added position to both of the image

--- src/Components/Banner2/Banner2.jsx
+++ src/Components/Banner2/Banner2.jsx
@@ -1,15 +1,17 @@
-import shape3 from '../../assets/shape3.png'
-import hero1 from '../../assets/hero1.png'
-import hero2 from '../../assets/hero2.png'
+import shape3 from '../../assets/shape3.png';
+import hero1 from '../../assets/hero1.png';
+import hero2 from '../../assets/hero2.png';
 
 const Banner2 = () => {
   return (
     <div>
       <div className="relative">
         <img src={shape3} alt="" />
-        <div className="flex items-center absolute">
-          <img className="w-[300px] h-[400px]" src={hero1} alt="" />
-          <img className="w-[300px] h-[400px]" src={hero2} alt="" />
+        <div className=" w-2/4 absolute top-[100px] left-[329px]">
+          <div className="flex items-center justify-between">
+            <img className="w-[300px] h-[400px]" src={hero1} alt="" />
+            <img className="w-[300px] h-[400px]" src={hero2} alt="" />
+          </div>
         </div>
       </div>
     </div>
