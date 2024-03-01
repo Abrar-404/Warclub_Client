f5e95b033b80ae4173e411fd59166a2f42cafe84
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-01 23:07:28 +0600

    added mobile position

--- src/Components/Banner2/Banner2.jsx
+++ src/Components/Banner2/Banner2.jsx
@@ -2,8 +2,7 @@ import shape3 from '../../assets/shape3.png';
 import hero1 from '../../assets/hero1.png';
 import hero2 from '../../assets/hero2.png';
 import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
+import 'aos/dist/aos.css';
 
 const Banner2 = () => {
   return (
@@ -13,7 +12,7 @@ const Banner2 = () => {
 
         <div className="">
           <img
-            className="absolute lg:w-[200px] lg:h-[300px] md:w-[150px] md:h-[170px] lg:top-[120px] lg:left-[200px] md:top-[68px] md:left-[100px]"
+            className="absolute lg:w-[250px] lg:h-[300px] md:w-[150px] md:h-[170px] lg:top-[120px] lg:left-[200px] md:top-[68px] md:left-[100px] w-[80px] h-[100px] top-[32px] "
             src={hero1}
             alt=""
           />
