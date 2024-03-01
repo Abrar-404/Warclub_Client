02ad31473a6757de582237441db68ac8ec49ecc7
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-01 21:39:33 +0600

    added mid w and h

--- src/Components/Banner2/Banner2.jsx
+++ src/Components/Banner2/Banner2.jsx
@@ -9,8 +9,16 @@ const Banner2 = () => {
         <img src={shape3} alt="" />
         <div className=" w-2/4 absolute lg:top-[100px] lg:left-[329px]">
           <div className="flex items-center justify-between">
-            <img className="lg:w-[300px] lg:h-[400px]" src={hero1} alt="" />
-            <img className="lg:w-[300px] lg:h-[400px]" src={hero2} alt="" />
+            <img
+              className="lg:w-[300px] lg:h-[400px] md:w-[200px] md:h-[200px]"
+              src={hero1}
+              alt=""
+            />
+            <img
+              className="lg:w-[300px] lg:h-[400px] md:w-[200px] md:h-[200px]"
+              src={hero2}
+              alt=""
+            />
           </div>
         </div>
       </div>
