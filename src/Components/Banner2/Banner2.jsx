07fbc5075229bbad05fde09ffb27f6925dd069c4
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-02 22:24:26 +0600

    bought jsx code

--- src/Components/Banner2/Banner2.jsx
+++ src/Components/Banner2/Banner2.jsx
@@ -42,14 +42,14 @@ const Banner2 = () => {
 
         <div class="box">
           <button class="bitti">SPACE</button>
-          <div class="space">
-            <span style="--i: 31" class="star"></span>
-            <span style="--i: 12" class="star"></span>
-            <span style="--i: 57" class="star"></span>
-            <span style="--i: 93" class="star"></span>
-            <span style="--i: 23" class="star"></span>
-            <span style="--i: 70" class="star"></span>
-            <span style="--i: 6" class="star"></span>
+          <div className="space">
+            <span style={{ '--i': 31 }} className="star"></span>
+            <span style={{ '--i': 12 }} className="star"></span>
+            <span style={{ '--i': 57 }} className="star"></span>
+            <span style={{ '--i': 93 }} className="star"></span>
+            <span style={{ '--i': 23 }} className="star"></span>
+            <span style={{ '--i': 70 }} className="star"></span>
+            <span style={{ '--i': 6 }} className="star"></span>
           </div>
         </div>
       </div>
