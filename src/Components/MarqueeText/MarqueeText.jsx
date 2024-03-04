20df365e8aab1178cb5a974f97a21f27cb8f68fa
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-04 22:31:16 +0600

    added 4th text size

--- src/Components/MarqueeText/MarqueeText.jsx
+++ src/Components/MarqueeText/MarqueeText.jsx
@@ -8,7 +8,7 @@ const MarqueeText = () => {
           <h1 className="text-4xl">ACTION-PACKED</h1>
           <h1 className="text-4xl">MIND-BENDING</h1>
           <h1 className="text-4xl">Collection OG Games</h1>
-          <h1>Gaming Spanning</h1>
+          <h1 className="text-4xl">Gaming Spanning</h1>
         </div>
       </Marquee>
     </div>
