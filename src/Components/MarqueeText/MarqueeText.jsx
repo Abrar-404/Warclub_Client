a83a354cb04baafa9d8a2f316b06c1120d2972ef
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-04 22:12:24 +0600

    added marquee text effect

---
+++ src/Components/MarqueeText/MarqueeText.jsx
@@ -0,0 +1,15 @@
+import Marquee from 'react-fast-marquee';
+
+const MarqueeText = () => {
+  return (
+    <div>
+      <Marquee>
+haha
+haha
+haha
+      </Marquee>
+    </div>
+  );
+};
+
+export default MarqueeText;
--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from '../../Banner/Banner';
 import Banner2 from '../../Banner2/Banner2';
+import MarqueeText from '../../MarqueeText/MarqueeText';
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Banner2></Banner2>
+      <MarqueeText></MarqueeText>
       <Banner></Banner>
     </div>
   );
