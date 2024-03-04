63fa5ae3037edbd6188e6047833739e1f795342d
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-04 22:54:22 +0600

    added marquee effect

--- src/Components/MarqueeText/MarqueeText.jsx
+++ src/Components/MarqueeText/MarqueeText.jsx
@@ -4,35 +4,35 @@ import star from '../../assets/star.png';
 const MarqueeText = () => {
   return (
     <div>
-      <Marquee className="lg:mt-10 md:mt-48 mt-96">
-        <div className="flex items-center lg:gap-10 md:gap-20 mx-auto">
-          <div className="flex items-center gap-5">
-            <h1 className="text-4xl text-[#45F882] hover:text-white font-extrabold">
+      <Marquee className="lg:mt-10 md:mt-48 mt-96 mx-auto">
+        <div className="flex items-center lg:gap-10 md:gap-10 mx-auto">
+          <div className="flex items-center gap-5 mx-auto">
+            <h1 className="lg:text-4xl text-xl text-[#45F882] hover:text-white font-extrabold overflow-hidden ml-5">
               ACTION-PACKED
             </h1>
-            <img src={star} alt="" />
           </div>
+          <img className="mr-5" src={star} alt="" />
 
-          <div className="flex items-center gap-5">
-            <h1 className="text-4xl text-[#45F882] hover:text-white font-extrabold">
+          <div className="flex items-center gap-5 mx-auto">
+            <h1 className="lg:text-4xl text-xl text-[#45F882] hover:text-white font-extrabold overflow-hidden">
               MIND-BENDING
             </h1>
-            <img src={star} alt="" />
           </div>
+          <img className="pr-2" src={star} alt="" />
 
-          <div className="flex items-center gap-5">
-            <h1 className="text-4xl text-[#45F882] hover:text-white font-extrabold">
+          <div className="flex items-center gap-5 mx-auto">
+            <h1 className="lg:text-4xl text-xl text-[#45F882] hover:text-white font-extrabold overflow-hidden">
               Collection OG Games
             </h1>
-            <img src={star} alt="" />
           </div>
+          <img className="pr-2" src={star} alt="" />
 
-          <div className="flex items-center gap-5">
-            <h1 className="text-4xl text-[#45F882] hover:text-white font-extrabold">
+          <div className="flex items-center gap-5 mx-auto">
+            <h1 className="lg:text-4xl text-xl text-[#45F882] hover:text-white font-extrabold overflow-hidden">
               Gaming Spanning
             </h1>
-            <img src={star} alt="" />
           </div>
+          <img className="pr-2" src={star} alt="" />
         </div>
       </Marquee>
     </div>
