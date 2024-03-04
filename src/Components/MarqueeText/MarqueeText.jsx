564e782b1e9cd8f6cf621b336ada534b75547c59
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-04 22:36:07 +0600

    added text hover effect

--- src/Components/MarqueeText/MarqueeText.jsx
+++ src/Components/MarqueeText/MarqueeText.jsx
@@ -3,9 +3,9 @@ import Marquee from 'react-fast-marquee';
 const MarqueeText = () => {
   return (
     <div>
-      <Marquee>
+      <Marquee className="">
         <div className="flex items-center gap-10">
-          <h1 className="text-4xl">ACTION-PACKED</h1>
+          <h1 className="text-4xl text-[#45F882] hover:text-white">ACTION-PACKED</h1>
           <h1 className="text-4xl">MIND-BENDING</h1>
           <h1 className="text-4xl">Collection OG Games</h1>
           <h1 className="text-4xl">Gaming Spanning</h1>
