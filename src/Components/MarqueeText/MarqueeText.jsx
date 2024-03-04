cae65272951ebacd236e4dbde7a84890b61638f7
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-04 22:29:01 +0600

    added text size 1

--- src/Components/MarqueeText/MarqueeText.jsx
+++ src/Components/MarqueeText/MarqueeText.jsx
@@ -4,7 +4,7 @@ const MarqueeText = () => {
   return (
     <div>
       <Marquee>
-        <h1>ACTION-PACKED</h1>
+        <h1 className='text-4xl'>ACTION-PACKED</h1>
         <h1>MIND-BENDING</h1>
         <h1>Collection OG Games</h1>
         <h1>Gaming Spanning</h1>
