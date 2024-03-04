f92424ae1db04ecd585e3efb29246241fe7818a2
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-04 22:41:58 +0600

    added font extra bold

--- src/Components/MarqueeText/MarqueeText.jsx
+++ src/Components/MarqueeText/MarqueeText.jsx
@@ -3,9 +3,11 @@ import Marquee from 'react-fast-marquee';
 const MarqueeText = () => {
   return (
     <div>
-      <Marquee className="">
-        <div className="flex items-center gap-10">
-          <h1 className="text-4xl text-[#45F882] hover:text-white">ACTION-PACKED</h1>
+      <Marquee className="lg:mt-10 md:mt-48 mt-96">
+        <div className="flex items-center lg:gap-10 md:gap-20 mx-auto">
+          <h1 className="text-4xl text-[#45F882] hover:text-white font-extrabold">
+            ACTION-PACKED
+          </h1>
           <h1 className="text-4xl">MIND-BENDING</h1>
           <h1 className="text-4xl">Collection OG Games</h1>
           <h1 className="text-4xl">Gaming Spanning</h1>
