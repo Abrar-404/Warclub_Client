6b66e7b414c2f278ec24ad32dc4da206c4190663
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-04 22:44:28 +0600

    added first star

--- src/Components/MarqueeText/MarqueeText.jsx
+++ src/Components/MarqueeText/MarqueeText.jsx
@@ -6,10 +6,11 @@ const MarqueeText = () => {
     <div>
       <Marquee className="lg:mt-10 md:mt-48 mt-96">
         <div className="flex items-center lg:gap-10 md:gap-20 mx-auto">
-          <div>
+          <div className='flex items-center gap-5'>
             <h1 className="text-4xl text-[#45F882] hover:text-white font-extrabold">
               ACTION-PACKED
             </h1>
+            <img src={star} alt="" />
           </div>
           <h1 className="text-4xl text-[#45F882] hover:text-white font-extrabold">
             MIND-BENDING
