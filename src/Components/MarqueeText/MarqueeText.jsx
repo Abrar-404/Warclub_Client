520dc469d67cc4d6e4a099591d5ba0c8239f9459
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-04 22:43:49 +0600

    imported star

--- src/Components/MarqueeText/MarqueeText.jsx
+++ src/Components/MarqueeText/MarqueeText.jsx
@@ -1,16 +1,25 @@
 import Marquee from 'react-fast-marquee';
+import star from '../../assets/star.png'
 
 const MarqueeText = () => {
   return (
     <div>
       <Marquee className="lg:mt-10 md:mt-48 mt-96">
         <div className="flex items-center lg:gap-10 md:gap-20 mx-auto">
+          <div>
+            <h1 className="text-4xl text-[#45F882] hover:text-white font-extrabold">
+              ACTION-PACKED
+            </h1>
+          </div>
           <h1 className="text-4xl text-[#45F882] hover:text-white font-extrabold">
-            ACTION-PACKED
+            MIND-BENDING
           </h1>
-          <h1 className="text-4xl">MIND-BENDING</h1>
-          <h1 className="text-4xl">Collection OG Games</h1>
-          <h1 className="text-4xl">Gaming Spanning</h1>
+          <h1 className="text-4xl text-[#45F882] hover:text-white font-extrabold">
+            Collection OG Games
+          </h1>
+          <h1 className="text-4xl text-[#45F882] hover:text-white font-extrabold">
+            Gaming Spanning
+          </h1>
         </div>
       </Marquee>
     </div>
