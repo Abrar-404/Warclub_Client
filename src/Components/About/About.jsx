5fa4839eb310c4e37117867b36c0cbea75ba5bc2
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-13 22:18:09 +0600

    added effect

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,10 +1,10 @@
 import bigPic from '../../assets/About.png';
-import littlePic from '../../assets/about2.png'
+import littlePic from '../../assets/about2.png';
 
 const About = () => {
   return (
     <div>
-      <div className='flex flex-col md:flex-col lg:flex-row mx-auto'>
+      <div className="flex flex-col md:flex-col lg:flex-row mx-auto ">
         <div>
           <img src={bigPic} alt="" />
         </div>
