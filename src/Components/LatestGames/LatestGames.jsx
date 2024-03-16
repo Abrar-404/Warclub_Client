6789fc7b70c3ba34300b801208efdb38c47c88d9
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-16 20:32:33 +0600

    imported css

--- src/Components/LatestGames/LatestGames.jsx
+++ src/Components/LatestGames/LatestGames.jsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-import './styles.css';
+import '../Styles/latestgames.css';
 
 
 import { Pagination } from 'swiper/modules';
