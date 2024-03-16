8781027a24efdb3a3b205a0fad85726bfd13b220
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-16 20:34:06 +0600

    added feature 1 to first slider

--- src/Components/LatestGames/LatestGames.jsx
+++ src/Components/LatestGames/LatestGames.jsx
@@ -41,7 +41,7 @@ export default function LatestGames() {
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide><img src={feature1} alt="" /></SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
