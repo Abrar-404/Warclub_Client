b7e730081d757e9d56c1e72f23e187e0f2fe99d1
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-16 20:42:01 +0600

    added height and width

--- src/Components/LatestGames/LatestGames.jsx
+++ src/Components/LatestGames/LatestGames.jsx
@@ -1,16 +1,14 @@
-
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-import latest1 from '../../assets/latest1 (1).png'
-import latest2 from '../../assets/latest2 (2).png'
-import latest3 from '../../assets/latest3 (3).png'
-import latest4 from '../../assets/latest4 (4).png'
+import latest1 from '../../assets/latest1 (1).png';
+import latest2 from '../../assets/latest2 (2).png';
+import latest3 from '../../assets/latest3 (3).png';
+import latest4 from '../../assets/latest4 (4).png';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
 
 import '../Styles/latestgames.css';
-
 
 import { Pagination } from 'swiper/modules';
 
@@ -50,13 +48,24 @@ export default function LatestGames() {
         <SwiperSlide>
           <img src={latest2} alt="" />
         </SwiperSlide>
-        <SwiperSlide><img src={latest3} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={latest4} alt="" /></SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>
+          <img src={latest3} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={latest4} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={latest1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={latest2} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={latest3} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="" src={latest4} alt="" />
+        </SwiperSlide>
       </Swiper>
     </latestgames>
   );
