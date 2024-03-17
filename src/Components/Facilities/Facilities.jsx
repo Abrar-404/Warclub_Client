04301066594434236e4e435e16ab812581d89f5b
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-17 19:13:52 +0600

    imported controller

--- src/Components/Facilities/Facilities.jsx
+++ src/Components/Facilities/Facilities.jsx
@@ -1,9 +1,11 @@
+import controller from '../../assets/controller.svg'
+
 const Facilities = () => {
   return (
     <div>
       <div>
         <div>
-          
+<img src={controller} alt="" />
         </div>
       </div>
     </div>
