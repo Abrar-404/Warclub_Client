f5e0f15eddf4b6260f12245b9fa20d25638a4b6e
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-17 18:45:48 +0600

    added 4th img

--- src/Components/LatestGames/LatestGames.jsx
+++ src/Components/LatestGames/LatestGames.jsx
@@ -72,8 +72,27 @@ export default function LatestGames() {
           </SwiperSlide>
 
           <SwiperSlide className="border-2 px-5 rounded-xl py-5 border-[#F03AF9]">
-            <div className="hover-zoom rounded-xl">
-              <img src={latest1} alt="" />
+            <div>
+              <div className="hover-zoom rounded-xl relative">
+                <img
+                  className="lg:h-[250px] lg:w-[250px] w-[120px] h-[75px]"
+                  src={latest1}
+                  alt=""
+                />
+              </div>
+
+              <div className="absolute top-[246px] left-[120px] mx-auto">
+                <img className="w-[60px] h-[50px]" src={liitle1} alt="" />
+              </div>
+
+              <div className="mt-10">
+                <h1 className="text-white hover:text-[#45F882] lg:text-2xl text-xs font-semibold">
+                  Ghostwire Tokyo
+                </h1>
+                <h1 className="text-white lg:text-base font-semibold text-xs">
+                  Entry Fee <span className="text-[#45F882]">$10.00</span>
+                </h1>
+              </div>
             </div>
           </SwiperSlide>
 
@@ -88,7 +107,7 @@ export default function LatestGames() {
               </div>
 
               <div className="absolute top-[246px] left-[120px] mx-auto">
-                <img className="w-[60px] h-[50px]" src={liitle2} alt="" />
+                <img className="w-[60px] h-[50px]" src={liitle3} alt="" />
               </div>
 
               <div className="mt-10">
