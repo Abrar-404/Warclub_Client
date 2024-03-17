09b6901277a352aac6628affe4a829074d221a39
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-17 19:14:55 +0600

    added texts in facilities

--- src/Components/Facilities/Facilities.jsx
+++ src/Components/Facilities/Facilities.jsx
@@ -5,7 +5,15 @@ const Facilities = () => {
     <div>
       <div>
         <div>
-<img src={controller} alt="" />
+          <img src={controller} alt="" />
+          <div>
+            <h1>Esports Lounge</h1> <br />
+            <h1>
+              Lounge areas with comfortable seating for relaxation between gaming
+              sessions. Provide snacks, drink, and vending machine for
+              refreshments.
+            </h1>
+          </div>
         </div>
       </div>
     </div>
