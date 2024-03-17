a490181b1b9a6352f089a6a7ae2f1601c7e0538c
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-17 19:22:00 +0600

    added facilities css

--- src/Components/Facilities/Facilities.jsx
+++ src/Components/Facilities/Facilities.jsx
@@ -1,18 +1,38 @@
-import controller from '../../assets/controller.svg'
+import controller from '../../assets/controller.svg';
 
 const Facilities = () => {
   return (
     <div>
-      <div>
+      <div className="flex justify-center mx-auto gap-10">
+        <div className="bg-[#0F1C23] rounded-xl bg-transparent">
+          <div className="py-5 px-5">
+            <div className="mx-auto  flex justify-center">
+              <img src={controller} alt="" />
+            </div>
+            <div className="">
+              <h1 className="text-center invisible">Esports Lounge</h1> <br />
+              <h1 className="text-center invisible">
+                Lounge areas with comfortable seating for relaxation between
+                gaming sessions. Provide snacks, drink, and vending machine for
+                refreshments.
+              </h1>
+            </div>
+          </div>
+        </div>
+
         <div className="bg-[#0F1C23] rounded-xl">
-          <img src={controller} alt="" />
-          <div>
-            <h1>Esports Lounge</h1> <br />
-            <h1>
-              Lounge areas with comfortable seating for relaxation between
-              gaming sessions. Provide snacks, drink, and vending machine for
-              refreshments.
-            </h1>
+          <div className="py-5 px-5">
+            <div className="mx-auto flex justify-center">
+              <img src={controller} alt="" />
+            </div>
+            <div className="">
+              <h1 className="text-center">Esports Lounge</h1> <br />
+              <h1 className="text-center">
+                Lounge areas with comfortable seating for relaxation between
+                gaming sessions. Provide snacks, drink, and vending machine for
+                refreshments.
+              </h1>
+            </div>
           </div>
         </div>
       </div>
