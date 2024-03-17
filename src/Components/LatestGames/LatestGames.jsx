0d5cc38b9534b132424964991ad3e86fe415253e
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-17 12:45:07 +0600

    added font-semibold

--- src/Components/LatestGames/LatestGames.jsx
+++ src/Components/LatestGames/LatestGames.jsx
@@ -97,7 +97,7 @@ export default function LatestGames() {
             </div>
 
             <div className="mt-5">
-              <h1 className="text-white text-2xl">The Hunter Killer</h1>
+              <h1 className="text-white text-2xl font-semibold">The Hunter Killer</h1>
               <h1 className="text-white text-base font-semibold">
                 Entry Fee <span className="text-[#45F882]">$10.00</span>
               </h1>
