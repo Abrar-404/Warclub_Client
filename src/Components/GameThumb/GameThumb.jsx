37eec8750f4bbef49273ddda0ec7032d9836255d
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-18 21:16:24 +0600

    readded revised code

--- src/Components/GameThumb/GameThumb.jsx
+++ src/Components/GameThumb/GameThumb.jsx
@@ -1,17 +1,7 @@
 import React, { useEffect } from 'react';
-import SwiperCore from 'swiper/core';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
-import './GameThumb.scss';
+import Swiper from 'swiper';
 
-import 'swiper/swiper.scss'; // Import Swiper styles
-
-import 'swiper/components/pagination/pagination.scss'; // Import Swiper pagination styles
-import 'swiper/components/navigation/navigation.scss'; // Import Swiper navigation styles
-
-SwiperCore.use([Navigation, Pagination]);
-
-function GameThumb() {
+const GameThumb = () => {
   useEffect(() => {
     const sliderSelector = '.swiper-container';
     const options = {
@@ -26,7 +16,7 @@ function GameThumb() {
         rotate: 50, // Slide rotate in degrees
         stretch: 0, // Stretch space between slides (in px)
         depth: 100, // Depth offset in px (slides translate in Z axis)
-        modifier: 1, // Effect multipler
+        modifier: 1, // Effect multiplier
         slideShadows: true, // Enables slides shadows
       },
       grabCursor: true,
@@ -53,9 +43,7 @@ function GameThumb() {
       },
     };
 
-    const mySwiper = new SwiperCore(sliderSelector, options);
-
-    // Initialize slider
+    const mySwiper = new Swiper(sliderSelector, options);
     mySwiper.init();
 
     // Cleanup function
@@ -66,16 +54,10 @@ function GameThumb() {
 
   return (
     <section className="swiper-container loading">
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        pagination={{ clickable: true }}
-        navigation={{
-          prevEl: '.swiper-button-prev',
-          nextEl: '.swiper-button-next',
-        }}
-      >
-        <SwiperSlide
+      <div className="swiper-wrapper">
+        {/* First slide */}
+        <div
+          className="swiper-slide"
           style={{
             backgroundImage:
               'url(https://images.unsplash.com/photo-1483985988355-763728e1935b?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MDE1NTg2Ng&ixlib=rb-1.2.1&q=85)',
@@ -99,15 +81,44 @@ function GameThumb() {
               scrambled it to make a type specimen book.
             </span>
           </div>
-        </SwiperSlide>
-        {/* Repeat similar structure for other slides */}
-      </Swiper>
+        </div>
+        {/* Second slide */}
+        <div
+          className="swiper-slide"
+          style={{
+            backgroundImage:
+              'url(https://images.unsplash.com/photo-1500643752441-4dc90cda350a?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MDE1NTg2Ng&ixlib=rb-1.2.1&q=85)',
+          }}
+        >
+          <img
+            src="https://images.unsplash.com/photo-1500643752441-4dc90cda350a?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MDE1NTg2Ng&ixlib=rb-1.2.1&q=85"
+            className="entity-img"
+          />
+          <div className="content">
+            <p
+              className="title"
+              data-swiper-parallax="-30%"
+              data-swiper-parallax-scale=".7"
+            >
+              Alexis Berry
+            </p>
+            <span className="caption" data-swiper-parallax="-20%">
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book.
+            </span>
+          </div>
+        </div>
+        {/* More slides */}
+      </div>
 
+      {/* If we need pagination */}
       <div className="swiper-pagination"></div>
+      {/* If we need navigation buttons */}
       <div className="swiper-button-prev swiper-button-white"></div>
       <div className="swiper-button-next swiper-button-white"></div>
     </section>
   );
-}
+};
 
 export default GameThumb;
