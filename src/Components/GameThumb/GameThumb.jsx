9fd5c1acdea725b757658d3a9512e1c64bffd96e
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-18 21:17:50 +0600

    added gamethumbcomp

--- src/Components/GameThumb/GameThumb.jsx
+++ src/Components/GameThumb/GameThumb.jsx
@@ -53,71 +53,73 @@ const GameThumb = () => {
   }, []);
 
   return (
-    <section className="swiper-container loading">
-      <div className="swiper-wrapper">
-        {/* First slide */}
-        <div
-          className="swiper-slide"
-          style={{
-            backgroundImage:
-              'url(https://images.unsplash.com/photo-1483985988355-763728e1935b?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MDE1NTg2Ng&ixlib=rb-1.2.1&q=85)',
-          }}
-        >
-          <img
-            src="https://images.unsplash.com/photo-1483985988355-763728e1935b?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MDE1NTg2Ng&ixlib=rb-1.2.1&q=85"
-            className="entity-img"
-          />
-          <div className="content">
-            <p
-              className="title"
-              data-swiper-parallax="-30%"
-              data-swiper-parallax-scale=".7"
-            >
-              Shaun Matthews
-            </p>
-            <span className="caption" data-swiper-parallax="-20%">
-              Lorem Ipsum has been the industry's standard dummy text ever since
-              the 1500s, when an unknown printer took a galley of type and
-              scrambled it to make a type specimen book.
-            </span>
+    <gamethumbcomp>
+      <section className="swiper-container loading">
+        <div className="swiper-wrapper">
+          {/* First slide */}
+          <div
+            className="swiper-slide"
+            style={{
+              backgroundImage:
+                'url(https://images.unsplash.com/photo-1483985988355-763728e1935b?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MDE1NTg2Ng&ixlib=rb-1.2.1&q=85)',
+            }}
+          >
+            <img
+              src="https://images.unsplash.com/photo-1483985988355-763728e1935b?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MDE1NTg2Ng&ixlib=rb-1.2.1&q=85"
+              className="entity-img"
+            />
+            <div className="content">
+              <p
+                className="title"
+                data-swiper-parallax="-30%"
+                data-swiper-parallax-scale=".7"
+              >
+                Shaun Matthews
+              </p>
+              <span className="caption" data-swiper-parallax="-20%">
+                Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book.
+              </span>
+            </div>
           </div>
-        </div>
-        {/* Second slide */}
-        <div
-          className="swiper-slide"
-          style={{
-            backgroundImage:
-              'url(https://images.unsplash.com/photo-1500643752441-4dc90cda350a?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MDE1NTg2Ng&ixlib=rb-1.2.1&q=85)',
-          }}
-        >
-          <img
-            src="https://images.unsplash.com/photo-1500643752441-4dc90cda350a?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MDE1NTg2Ng&ixlib=rb-1.2.1&q=85"
-            className="entity-img"
-          />
-          <div className="content">
-            <p
-              className="title"
-              data-swiper-parallax="-30%"
-              data-swiper-parallax-scale=".7"
-            >
-              Alexis Berry
-            </p>
-            <span className="caption" data-swiper-parallax="-20%">
-              Lorem Ipsum has been the industry's standard dummy text ever since
-              the 1500s, when an unknown printer took a galley of type and
-              scrambled it to make a type specimen book.
-            </span>
+          {/* Second slide */}
+          <div
+            className="swiper-slide"
+            style={{
+              backgroundImage:
+                'url(https://images.unsplash.com/photo-1500643752441-4dc90cda350a?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MDE1NTg2Ng&ixlib=rb-1.2.1&q=85)',
+            }}
+          >
+            <img
+              src="https://images.unsplash.com/photo-1500643752441-4dc90cda350a?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTY0MDE1NTg2Ng&ixlib=rb-1.2.1&q=85"
+              className="entity-img"
+            />
+            <div className="content">
+              <p
+                className="title"
+                data-swiper-parallax="-30%"
+                data-swiper-parallax-scale=".7"
+              >
+                Alexis Berry
+              </p>
+              <span className="caption" data-swiper-parallax="-20%">
+                Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book.
+              </span>
+            </div>
           </div>
+          {/* More slides */}
         </div>
-        {/* More slides */}
-      </div>
 
-      {/* If we need pagination */}
-      <div className="swiper-pagination"></div>
-      {/* If we need navigation buttons */}
-      <div className="swiper-button-prev swiper-button-white"></div>
-      <div className="swiper-button-next swiper-button-white"></div>
-    </section>
+        {/* If we need pagination */}
+        <div className="swiper-pagination"></div>
+        {/* If we need navigation buttons */}
+        <div className="swiper-button-prev swiper-button-white"></div>
+        <div className="swiper-button-next swiper-button-white"></div>
+      </section>
+    </gamethumbcomp>
   );
 };
 
