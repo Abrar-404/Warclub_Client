cc658f8de721d8b21461fcad7809f8bd8bd67344
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-18 21:03:56 +0600

    added demo img

--- src/Components/GameThumb/GameThumb.jsx
+++ src/Components/GameThumb/GameThumb.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import Swiper from 'swiper';
 import 'swiper/swiper-bundle.css';
 import './GameThumb.css'
+import img1 from '../../assets/About.png'
 
 const GameThumb = () => {
   useEffect(() => {
