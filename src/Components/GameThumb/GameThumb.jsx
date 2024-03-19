d0cfd291221de1c381ed1ac36a5ddce6e6fdf7ed
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-19 19:56:39 +0600

    added btn to 5th pic

--- src/Components/GameThumb/GameThumb.jsx
+++ src/Components/GameThumb/GameThumb.jsx
@@ -104,11 +104,16 @@ function GameThumb() {
           </SwiperSlide>
 
           <SwiperSlide className="h-[200px] w-[300px]">
-            <img
-              className="lg:h-[400px] lg:w-[600px] md:h-[200px] md:w-[400px] h-[100px] w-[300px] border-2 border-green-600 rounded-xl"
-              src={img5}
-              alt="slide_image"
-            />
+            <div className="relative">
+              <img
+                className="lg:h-[400px] lg:w-[600px] md:h-[200px] md:w-[400px] h-[100px] w-[300px] border-2 border-green-600 rounded-xl"
+                src={img5}
+                alt="slide_image"
+              />
+              <div className="absolute top-[130px] left-[40%]">
+                <PulseButton></PulseButton>
+              </div>
+            </div>
           </SwiperSlide>
 
           <SwiperSlide className="h-[200px] w-[300px]">
