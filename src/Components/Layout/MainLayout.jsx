31aecea84b0391ef7daffd2e19cd7bbd82f62be0
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-19 20:07:26 +0600

    added max w and h

--- src/Components/Layout/MainLayout.jsx
+++ src/Components/Layout/MainLayout.jsx
@@ -5,28 +5,30 @@ import AnimatedCursor from 'react-animated-cursor';
 
 const MainLayout = () => {
   return (
-    <div className="h-cover bg-[url('https://themedox.com/demo/mykd/assets/img/bg/area_bg02.jpg')] svg mx-auto">
-      {/* <SmoothScroll></SmoothScroll> */}
+    <div className="md:max-w-[768px] max-w-[428px] lg:max-w-[1400px] mx-auto">
+      <div className="h-cover bg-[url('https://themedox.com/demo/mykd/assets/img/bg/area_bg02.jpg')] svg mx-auto">
+        {/* <SmoothScroll></SmoothScroll> */}
 
-      <AnimatedCursor
-        innerSize={10}
-        outerSize={50}
-        innerScale={1}
-        outerScale={2}
-        outerAlpha={0}
-        trailingSpeed={4}
-        hasBlendMode={true}
-        innerStyle={{
-          backgroundColor: 'white',
-        }}
-        outerStyle={{
-          border: '3px solid white',
-        }}
-      />
+        <AnimatedCursor
+          innerSize={10}
+          outerSize={50}
+          innerScale={1}
+          outerScale={2}
+          outerAlpha={0}
+          trailingSpeed={4}
+          hasBlendMode={true}
+          innerStyle={{
+            backgroundColor: 'white',
+          }}
+          outerStyle={{
+            border: '3px solid white',
+          }}
+        />
 
-      <div>
-        <Navbar></Navbar>
-        <Outlet></Outlet>
+        <div>
+          <Navbar></Navbar>
+          <Outlet></Outlet>
+        </div>
       </div>
     </div>
   );
