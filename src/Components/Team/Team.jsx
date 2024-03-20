42d228d715fa6540e3339aef9ee0dfd5bf46f62f
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-20 13:06:00 +0600

    added position

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -6,8 +6,8 @@ const Team = () => {
       <div>
         <div className="relative">
           <img src={teamImg} alt="" />
-          <div className="absolute top-[20%]">
-            <h1 className="text-[#43F27F]"># World Best Gaming Site</h1> <br />
+          <div className="absolute lg:top-[20%] lg:left-[10%]">
+            <h1 className="text-[#43F27F] font-semibold"># World Best Gaming Site</h1> <br />
             <h1 className="text-white lg:text-3xl font-bold">
               Join Warclub Esports To Become <br /> Next{' '}
               <span className="text-[#43F27F] font-normal">
