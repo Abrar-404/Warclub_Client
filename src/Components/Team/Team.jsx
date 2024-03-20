3f29eba5a8d09af0440407689e7d0b33154e8f9d
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-20 13:04:30 +0600

    added relative class

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -1,16 +1,28 @@
-import teamImg from '../../assets/team.png'
+import teamImg from '../../assets/team.png';
 
 const Team = () => {
   return (
     <div>
       <div>
-        <img src={teamImg} alt="" />
+        <div className='relative'>
+          <img src={teamImg} alt="" />
+        </div>
 
-        <div>
+        <div className="absolute">
           <h1 className="text-[#43F27F]"># World Best Gaming Site</h1> <br />
-          <h1 className='text-white lg:text-3xl'>
-            Join Warclub Esports To Become Next{' '}
-            <span className="text-[#43F27F]">PRO Gamer Today !</span>
+          <h1 className="text-white lg:text-3xl font-bold">
+            Join Warclub Esports To Become <br /> Next{' '}
+            <span className="text-[#43F27F] font-normal">
+              PRO Gamer Today !
+            </span>
+          </h1>{' '}
+          <br />
+          <h1 className="text-[#6F7989]">
+            Esports and gaming facilities requires thoughtful consideration of{' '}
+            <br />
+            various elements to create an environment that caters to the needs{' '}
+            <br />
+            of gamers and enhances the overall gaming experience.
           </h1>
         </div>
       </div>
