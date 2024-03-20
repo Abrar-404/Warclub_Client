77a87224f06ebc7298cc2d397a2a49618dfc50f2
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-21 00:10:44 +0600

    added text services

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -27,27 +27,35 @@ const Footer = () => {
             </p>
           </aside>
           <nav>
-            <h6 className="footer-title">Services</h6>
+            <h6 className="font-bold text-2xl border-b-2 pb-1 border-green-400 text-white">
+              Services
+            </h6>
             <a className="link link-hover">Branding</a>
             <a className="link link-hover">Design</a>
             <a className="link link-hover">Marketing</a>
             <a className="link link-hover">Advertisement</a>
           </nav>
           <nav>
-            <h6 className="footer-title">Company</h6>
+            <h6 className="font-bold text-2xl border-b-2 pb-1 border-green-400 text-white">
+              Company
+            </h6>
             <a className="link link-hover">About us</a>
             <a className="link link-hover">Contact</a>
             <a className="link link-hover">Jobs</a>
             <a className="link link-hover">Press kit</a>
           </nav>
           <nav>
-            <h6 className="footer-title">Legal</h6>
+            <h6 className="font-bold text-2xl border-b-2 pb-1 border-green-400 text-white">
+              Legal
+            </h6>
             <a className="link link-hover">Terms of use</a>
             <a className="link link-hover">Privacy policy</a>
             <a className="link link-hover">Cookie policy</a>
           </nav>
           <form>
-            <h6 className="footer-title">Newsletter</h6>
+            <h6 className="font-bold text-2xl border-b-2 pb-1 border-green-400 text-white">
+              Newsletter
+            </h6>
             <fieldset className="form-control w-80">
               <label className="label">
                 <span className="label-text">Enter your email address</span>
