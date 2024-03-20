ab4310deab5b2947c8918663d3cf1e1e2be8a582
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-20 19:02:30 +0600

    added pic4

--- src/Components/BlogsSlider/BlogsSlider.jsx
+++ src/Components/BlogsSlider/BlogsSlider.jsx
@@ -54,40 +54,21 @@ export default function BlogSlider() {
             </div>
           </SwiperSlide>
 
-          <SwiperSlide className="border-2 px-5 rounded-xl py-5 border-[#0FA48D]">
+          <SwiperSlide className="px-5 rounded-xl py-5">
             <div>
               <img src={blogPic2} alt="" />
             </div>
           </SwiperSlide>
 
-          <SwiperSlide className="border-2 px-5 rounded-xl py-5 border-[#F03AF9]">
+          <SwiperSlide className="px-5 rounded-xl py-5">
             <div>
               <img src={blogPic3} alt="" />
             </div>
           </SwiperSlide>
 
-          <SwiperSlide className="border-2 px-5 rounded-xl py-5 border-[#E58829]">
+          <SwiperSlide className="px-5 rounded-xl py-5">
             <div>
-              <div className="hover-zoom rounded-xl relative">
-                <img
-                  className="lg:h-[250px] lg:w-[250px] w-[120px] h-[75px]"
-                  src={latest4}
-                  alt=""
-                />
-              </div>
-
-              <div className="absolute top-[246px] left-[120px] mx-auto">
-                <img className="w-[60px] h-[50px]" src={liitle2} alt="" />
-              </div>
-
-              <div className="mt-10">
-                <h1 className="text-white hover:text-[#45F882] lg:text-2xl text-xs font-semibold">
-                  The Hunter Killer
-                </h1>
-                <h1 className="text-white lg:text-base font-semibold text-xs">
-                  Entry Fee : <span className="text-[#45F882]">$10.00</span>
-                </h1>
-              </div>
+              <img src={blogPic4} alt="" />
             </div>
           </SwiperSlide>
 
