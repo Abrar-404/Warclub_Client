d6422fd2142312063bf91fe770c6d2d7b7c969f4
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-21 11:37:55 +0600

    imported arrow

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,12 +1,16 @@
+import { IoIosArrowForward } from 'react-icons/io';
+
 const AboutUs = () => {
   return (
     <div>
       <div>
-        <h1 className="text-white font-bold lg:text-3xl text-center">About Us</h1>
+        <h1 className="text-white font-bold lg:text-3xl text-center">
+          About Us
+        </h1>
 
         <div>
           <h1>Home </h1>
-      </div>
+        </div>
       </div>
     </div>
   );
