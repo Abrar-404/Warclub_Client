0542ac32e1e0d9f89b374ecbcc3e9b58896917a5
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-22 23:08:06 +0600

    added text center of second div

--- src/Components/AboutUs/AboutFooter.jsx
+++ src/Components/AboutUs/AboutFooter.jsx
@@ -1,4 +1,5 @@
 import footerBgPic from '../../assets/AboutFooter.png';
+import Socials from '../Footer/Socials';
 
 const AboutFooter = () => {
   return (
@@ -12,6 +13,56 @@ const AboutFooter = () => {
           <h1 className="text-white font-bold hover:text-[#45F882]">Blog</h1>
           <h1 className="text-white font-bold hover:text-[#45F882]">Gallery</h1>
           <h1 className="text-white font-bold hover:text-[#45F882]">Contact</h1>
+        </div>
+
+        <div className="flex justify-center mx-auto lg:flex-row md:flex-col flex-col gap-20 ml-10 mt-20">
+          <div>
+            <div>
+              <h1 className="text-white font-bold text-lg text-left">
+                Office <span className="text-[#45f882]">Location:</span>
+              </h1>
+            </div>
+
+            <div className="mt-2">
+              <h1 className="text-left text-gray-400">
+                Floor 2, 4 Naval St, Ancoats, Manchester <br /> M4 6EW, United
+                Kingdom
+              </h1>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <h1 className="text-white font-bold text-lg text-left">
+                Contact <span className="text-[#45f882]">Information:</span>
+              </h1>
+            </div>
+
+            <div className="mt-2">
+              <div>
+                <h1 className="text-left text-gray-400 hover:text-[#45f882]">
+                  (+163)-2547-36587
+                </h1>
+              </div>{' '}
+              <div>
+                <h1 className="text-left text-gray-400 hover:text-[#45f882]">
+                  infohelp@bameexample.com
+                </h1>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <h1 className="text-white font-bold text-lg text-left">
+                Follow <span className="text-[#45f882]">With Us:</span>
+              </h1>
+            </div>
+
+            <div>
+              <Socials></Socials>
+            </div>
+          </div>
         </div>
       </div>
     </div>
