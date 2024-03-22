a35ed1c6661e9d54029953686294ee40d9be69b1
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-22 19:13:43 +0600

    added text heading 2

--- src/Components/AboutUs/TourBanner.jsx
+++ src/Components/AboutUs/TourBanner.jsx
@@ -3,9 +3,15 @@ import '../Styles/tourbanner.css';
 const TourBanner = () => {
   return (
     <tourbanner>
-      <div>
+      <div className="mt-32">
         <div>
-          <h1>Lion</h1>
+          <h1 className="text-[#45f882] font-bold text-sm space-x-1">
+            # HISTORY OF WARCLUB TROPHY
+          </h1>
+        </div>
+
+        <div>
+          <h1>Our All Tournaments History !</h1>
         </div>
       </div>
     </tourbanner>
