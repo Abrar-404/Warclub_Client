9fa00d9caedbcb8c712ed593c36e610fff2e3c20
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-22 21:07:40 +0600

    added text circle

--- src/Components/AboutUs/Facts.jsx
+++ src/Components/AboutUs/Facts.jsx
@@ -2,92 +2,98 @@ import fact1pic1 from '../../assets/aboutusPic1.png';
 import fact1pic2 from '../../assets/aboutusPic2.png';
 
 import { IoIosCheckmarkCircle } from 'react-icons/io';
+import TextCircle from './TextCircle';
 
 const Facts = () => {
   return (
     <div className="mt-32">
-      <div className="flex justify-evenly lg:flex-row md:flex-col flex-col gap-10 mx-auto items-center">
-        <div>
+      <div>
+        <div className="flex justify-evenly lg:flex-row md:flex-col flex-col gap-10 mx-auto items-center">
           <div>
-            <h1 className="text-[#45F882] font-semibold lg:text-left md:text-center text-center text-base">
-              # Why Choose Our Gaming Site
-            </h1>
+            <div>
+              <h1 className="text-[#45F882] font-semibold lg:text-left md:text-center text-center text-base">
+                # Why Choose Our Gaming Site
+              </h1>
+            </div>
+            <div>
+              <h1 className="text-white lg:text-3xl md:text-2xl lg:text-left md:text-center text-center text-xl font-bold">
+                Our Values Inspire And Drive Our <br /> Every Move{' '}
+                <span className="text-[#45F882]">!</span>
+              </h1>
+            </div>
+
+            <div className="mt-5">
+              <div className="flex justify-start gap-2 mt-1 items-center">
+                <div className="text-[#45F882]">
+                  <IoIosCheckmarkCircle></IoIosCheckmarkCircle>
+                </div>
+                <h1 className="text-gray-400 font-semibold">
+                  {' '}
+                  Duis scelerisque nunc ac massa efficitur pulvinar.
+                </h1>
+              </div>
+              <div className="flex justify-start gap-2 mt-1 items-center">
+                <div className="text-[#45F882]">
+                  <IoIosCheckmarkCircle></IoIosCheckmarkCircle>
+                </div>
+                <h1 className="text-gray-400 font-semibold">
+                  {' '}
+                  Vivamus eget nisi scelerisque, iaculis risus vel, molestie
+                  risus.
+                </h1>
+              </div>
+              <div className="flex justify-start gap-2 mt-1 items-center">
+                <div className="text-[#45F882]">
+                  <IoIosCheckmarkCircle></IoIosCheckmarkCircle>
+                </div>
+                <h1 className="text-gray-400 font-semibold">
+                  {' '}
+                  Aliquam nec sapien vitae dui dapibus blandit.
+                </h1>
+              </div>
+              <div className="flex justify-start gap-2 mt-1 items-center">
+                <div className="text-[#45F882]">
+                  <IoIosCheckmarkCircle></IoIosCheckmarkCircle>
+                </div>
+                <h1 className="text-gray-400 font-semibold">
+                  {' '}
+                  Sed non ipsum ut mauris dictum ullamcorper ac at nibh.
+                </h1>
+              </div>
+            </div>
           </div>
+
           <div>
-            <h1 className="text-white lg:text-3xl md:text-2xl lg:text-left md:text-center text-center text-xl font-bold">
-              Our Values Inspire And Drive Our <br /> Every Move{' '}
-              <span className="text-[#45F882]">!</span>
-            </h1>
+            <img className="rounded-3xl" src={fact1pic2} alt="" />
+          </div>
+        </div>
+
+        <div className="flex justify-evenly lg:flex-row md:flex-col flex-col gap-10 mx-auto items-center mt-10">
+          <div>
+            <img className="rounded-3xl" src={fact1pic1} alt="" />
           </div>
 
-          <div className="mt-5">
-            <div className="flex justify-start gap-2 mt-1 items-center">
-              <div className="text-[#45F882]">
-                <IoIosCheckmarkCircle></IoIosCheckmarkCircle>
-              </div>
-              <h1 className="text-gray-400 font-semibold">
-                {' '}
-                Duis scelerisque nunc ac massa efficitur pulvinar.
-              </h1>
+          <div>
+            <div className="text-left text-gray-400 font-semibold">
+              Gamers can join local gaming meetups, participate in gaming <br />{' '}
+              events, or connect with like-minded individuals through online{' '}
+              <br /> forums and social media groups. Engaging with the community{' '}
+              <br /> helps build connections and creates opportunities for
+              collaborative <br /> gaming experiences.
             </div>
-            <div className="flex justify-start gap-2 mt-1 items-center">
-              <div className="text-[#45F882]">
-                <IoIosCheckmarkCircle></IoIosCheckmarkCircle>
-              </div>
-              <h1 className="text-gray-400 font-semibold">
-                {' '}
-                Vivamus eget nisi scelerisque, iaculis risus vel, molestie
-                risus.
-              </h1>
-            </div>
-            <div className="flex justify-start gap-2 mt-1 items-center">
-              <div className="text-[#45F882]">
-                <IoIosCheckmarkCircle></IoIosCheckmarkCircle>
-              </div>
-              <h1 className="text-gray-400 font-semibold">
-                {' '}
-                Aliquam nec sapien vitae dui dapibus blandit.
-              </h1>
-            </div>
-            <div className="flex justify-start gap-2 mt-1 items-center">
-              <div className="text-[#45F882]">
-                <IoIosCheckmarkCircle></IoIosCheckmarkCircle>
-              </div>
-              <h1 className="text-gray-400 font-semibold">
-                {' '}
-                Sed non ipsum ut mauris dictum ullamcorper ac at nibh.
-              </h1>
+
+            <div className="text-left mt-5 text-gray-400 font-semibold">
+              Storytelling is a crucial element in modern video game design,{' '}
+              <br />
+              creating immersive and engaging experiences. Games with <br />{' '}
+              compelling narratives often resonate more with players, adding{' '}
+              <br /> depth to the overall gaming experience.
             </div>
           </div>
         </div>
 
-        <div>
-          <img className="rounded-3xl" src={fact1pic2} alt="" />
-        </div>
-      </div>
 
-      <div className="flex justify-evenly lg:flex-row md:flex-col flex-col gap-10 mx-auto items-center mt-10">
-        <div>
-          <img className="rounded-3xl" src={fact1pic1} alt="" />
-        </div>
-
-        <div>
-          <div className="text-left text-gray-400 font-semibold">
-            Gamers can join local gaming meetups, participate in gaming <br />{' '}
-            events, or connect with like-minded individuals through online{' '}
-            <br /> forums and social media groups. Engaging with the community{' '}
-            <br /> helps build connections and creates opportunities for
-            collaborative <br /> gaming experiences.
-          </div>
-
-          <div className="text-left mt-5 text-gray-400 font-semibold">
-            Storytelling is a crucial element in modern video game design,{' '}
-            <br />
-            creating immersive and engaging experiences. Games with <br />{' '}
-            compelling narratives often resonate more with players, adding{' '}
-            <br /> depth to the overall gaming experience.
-          </div>
-        </div>
+        <TextCircle></TextCircle>
       </div>
     </div>
   );
