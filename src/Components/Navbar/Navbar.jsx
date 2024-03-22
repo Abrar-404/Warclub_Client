56303227ecce6e9ada1c1f5739b5dcead4ea7dc3
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-22 20:12:17 +0600

    added logo to footer

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -142,7 +142,7 @@ const Navbar = () => {
             </ul>
           </div>
           <Link to="/" className="normal-case text-xl flex items-center">
-            <img className="w-[220px] h-[100px]" src={logo} alt="" />
+            <img className="w-[250px] h-[100px]" src={logo} alt="" />
           </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
