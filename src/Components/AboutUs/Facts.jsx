0ba7ac6150302c682591c9edb2873338780e0b70
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-22 19:26:04 +0600

    added import pic

--- src/Components/AboutUs/Facts.jsx
+++ src/Components/AboutUs/Facts.jsx
@@ -1,7 +1,12 @@
+import fact1pic1 from '../../assets/aboutusPic1.png'
+import fact1pic2 from '../../assets/aboutusPic2.png'
+
 const Facts = () => {
   return (
     <div>
-      
+      <div>
+        
+      </div>
     </div>
   );
 };
