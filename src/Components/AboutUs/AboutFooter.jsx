cf8876636d49da243a861497faf90dfde29961fc
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-22 22:47:42 +0600

    added footer btns

---
+++ src/Components/AboutUs/AboutFooter.jsx
@@ -0,0 +1,17 @@
+const AboutFooter = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Home</h1>
+          <h1>About Us</h1>
+          <h1>Blog</h1>
+          <h1>Gallery</h1>
+          <h1>Contact</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutFooter;
