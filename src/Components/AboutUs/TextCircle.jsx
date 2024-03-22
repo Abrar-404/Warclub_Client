5ee3bb32d8b59d01395b57dbe0fe5b0cb5212231
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-22 21:20:37 +0600

    added text img

--- src/Components/AboutUs/TextCircle.jsx
+++ src/Components/AboutUs/TextCircle.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import '../Styles/circletext.css';
 import '../Styles/customFont.css';
+import logo from '../../assets/logosamp.png';
 
 const TextCircle = () => {
   useEffect(() => {
@@ -10,7 +11,7 @@ const TextCircle = () => {
         .split('')
         .map(
           (char, i) =>
-            `<span style="transform:rotate(${i * 10}deg)">${char}</span>`
+            `<span style="transform:rotate(${i * 25}deg)">${char}</span>`
         )
         .join('');
     } else {
@@ -22,18 +23,18 @@ const TextCircle = () => {
     <textcircle>
       <div className="flex md:flex-col lg:flex-row flex-col mx-auto gap-14 mt-72 justify-center">
         <div>
-          <div>
-            <div className="relative">
+          <div className="">
+            <div className="relative ">
               <div className="absolute lg:bottom-[70px] lg:right-[400px] f md:right-[470px] md:bottom-[50px] flex justify-center bottom-[60px] right-[90px]">
                 <div>
                   <div className="circle">
                     <img
                       className="rounded-full w-[150px] h-[150px]"
-                      src="https://media.istockphoto.com/id/1390233984/photo/modern-luxury-bedroom.jpg?s=612x612&w=0&k=20&c=po91poqYoQTbHUpO1LD1HcxCFZVpRG-loAMWZT7YRe4="
+                      src={logo}
                       alt=""
                     />
-                    <div className="text">
-                      <p>Experience Gam ing 24 Years With Warclub</p>
+                    <div className="text border-2 border-[#45F882] rounded-full">
+                      <p className="text-[#45F882] ">w a r c l u b</p>
                     </div>
                   </div>
                 </div>
