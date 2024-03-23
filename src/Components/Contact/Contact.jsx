75892ab94faf661c54d43ccf2a8e7901f19f5b6a
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-23 11:43:11 +0600

    added about bg

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,30 @@
+import aboutBg from '../../assets/aboutusbg.png'
+
 const Contact = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div>
+        <div style={{ backgroundImage: `url(${aboutBg})` }}>
+          <div className="mx-auto pt-32 pb-32">
+            <h1 className="text-white font-bold lg:text-3xl text-center">
+              About Us
+            </h1>
+
+            <div className="text-white font-semibold text-sm text-center flex justify-center mx-auto items-center pt-2">
+              <h1>
+                <Link to="/">
+                  <span className="text-green-400">Home</span>
+                </Link>{' '}
+              </h1>
+              <div>
+                <IoIosArrowForward></IoIosArrowForward>
+              </div>
+              <div>About Us</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
