0c698090b39ed3a4a66c56a5b7d6841b41e1b4fd
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-23 12:06:47 +0600

    added email js initials

--- src/Components/Contact/ContactForm.jsx
+++ src/Components/Contact/ContactForm.jsx
@@ -1,4 +1,43 @@
 const ContactForm = () => {
+
+    const form = useRef();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const sendEmail = e => {
+      e.preventDefault();
+
+      emailjs
+        .sendForm(
+          'service_vr7e1xj',
+          'template_zpyioxg',
+          form.current,
+          'QtXOYgGb2gzk-cpQc'
+        )
+        .then(
+          result => {
+            Swal.fire({
+              position: 'center',
+              icon: 'success',
+              title: 'Message Sent Successfully',
+              showConfirmButton: false,
+              background: '#2B3445',
+              timer: 1500,
+            });
+            console.log(result.text);
+          },
+          error => {
+            console.log(error.text);
+          }
+        );
+
+      // Reset the input fields
+      setName('');
+      setEmail('');
+      setMessage('');
+    };
+
   return (
     <div>
       <div>
@@ -12,6 +51,10 @@ const ContactForm = () => {
           <h1 className="text-white text-2xl font-bold">
             Contact Us & Meet Our Team <span className="text-[#45F882]">!</span>
           </h1>
+        </div>
+
+        <div>
+
         </div>
       </div>
     </div>
