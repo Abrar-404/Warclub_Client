55b4f2579605fd23d791aad47dddce1ff669c600
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-23 22:53:58 +0600

    added flex box of texts and review

--- src/Components/AllGames/AllGamesCard.jsx
+++ src/Components/AllGames/AllGamesCard.jsx
@@ -6,9 +6,9 @@ const AllGamesCard = ({ gamesItems }) => {
   return (
     <div>
       <div className="border border-gray-400 pt-20 pb-10 w-[22%] pl-5 rounded-3xl">
-        <div>
+        <div className="flex justify-center items-center mx-auto">
           <h1 className="text-white lg:text-xl font-semibold">{name}</h1>
-          <div className="flex justify-center items-center mx-auto gap-1">
+          <div className="flex justify-center items-center mx-auto gap-1 bg-[#0F1C23] w-[14%] rounded-sm">
             <div className="text-[8px] text-[#FFBE18]">
               <FaStar></FaStar>
             </div>
@@ -16,10 +16,13 @@ const AllGamesCard = ({ gamesItems }) => {
           </div>
         </div>
 
-        <div>
+        <div className="flex items-center gap-20 mx-auto">
           <h1 className="text-white text-xs font-medium">
             Entry Fee : <span className="text-[#3EDC75]  text-xs">${fee}</span>
           </h1>
+          <div>
+            <h1 className="text-[10px] text-gray-400">( {review}k Review) </h1>
+          </div>
         </div>
       </div>
     </div>
