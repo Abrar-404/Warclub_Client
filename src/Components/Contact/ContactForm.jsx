2a479e6d8db9066d7020da1b4bf15adbf2548a6a
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-23 12:04:33 +0600

    added font bold

--- src/Components/Contact/ContactForm.jsx
+++ src/Components/Contact/ContactForm.jsx
@@ -3,7 +3,15 @@ const ContactForm = () => {
     <div>
       <div>
         <div>
-          <h1 className="text-[#45F882] text-base font-semibold"># GET IN TOUCH</h1>
+          <h1 className="text-[#45F882] text-base font-semibold">
+            # GET IN TOUCH
+          </h1>
+        </div>
+
+        <div>
+          <h1 className="text-white text-2xl font-bold">
+            Contact Us & Meet Our Team <span className="text-[#45F882]">!</span>
+          </h1>
         </div>
       </div>
     </div>
