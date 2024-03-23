91c8b78c13e2c735f7c96409ff3ced2be492600a
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-23 22:20:50 +0600

    added card styles

--- src/Components/AllGames/AllGamesCard.jsx
+++ src/Components/AllGames/AllGamesCard.jsx
@@ -1,10 +1,12 @@
+import { FaStar } from 'react-icons/fa6';
+
 const AllGamesCard = ({ gamesItems }) => {
   const { img, name, review } = gamesItems || {};
 
   return (
     <div>
-      <div>
-        <h1>{name}</h1>
+      <div className="border border-gray-400 pt-20 pb-10 w-[25%] pl-5 rounded-3xl">
+        <h1 className="text-white lg:text-xl font-semibold">{name}</h1>
       </div>
     </div>
   );
