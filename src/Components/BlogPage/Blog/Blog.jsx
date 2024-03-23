9633f932036cfecd9639cf2bd402f5e4c71e459c
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-23 16:10:06 +0600

    added new comp

--- src/Components/BlogPage/Blog/Blog.jsx
+++ src/Components/BlogPage/Blog/Blog.jsx
@@ -1,28 +1,37 @@
 import { IoIosArrowForward } from "react-icons/io";
 import aboutBg from '../../../assets/aboutusbg.png'
 import { Link } from "react-router-dom";
+import BlogSearch from "./BlogSearch";
 
 const Blog = () => {
   return (
     <div>
-      <div style={{ backgroundImage: `url(${aboutBg})` }}>
-        <div className="mx-auto pt-32 pb-32">
-          <h1 className="text-white font-bold lg:text-3xl text-center">
-            Latest News
-          </h1>
+      <div>
 
-          <div className="text-white font-semibold text-sm text-center flex justify-center mx-auto items-center pt-2">
-            <h1>
-              <Link to="/">
-                <span className="text-green-400">Home</span>
-              </Link>{' '}
+        {/* top banner */}
+
+        <div style={{ backgroundImage: `url(${aboutBg})` }}>
+          <div className="mx-auto pt-32 pb-32">
+            <h1 className="text-white font-bold lg:text-3xl text-center">
+              Latest News
             </h1>
-            <div>
-              <IoIosArrowForward></IoIosArrowForward>
+
+            <div className="text-white font-semibold text-sm text-center flex justify-center mx-auto items-center pt-2">
+              <h1>
+                <Link to="/">
+                  <span className="text-green-400">Home</span>
+                </Link>{' '}
+              </h1>
+              <div>
+                <IoIosArrowForward></IoIosArrowForward>
+              </div>
+              <div>Blog</div>
             </div>
-            <div>Blog</div>
           </div>
         </div>
+
+
+        <BlogSearch></BlogSearch>
       </div>
     </div>
   );
---
+++ src/Components/BlogPage/Blog/BlogSearch.jsx
@@ -0,0 +1,5 @@
+const BlogSearch = () => {
+  return <div></div>;
+};
+
+export default BlogSearch;
