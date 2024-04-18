0383a4c5a1ec1102b00fd0729c9c38e9032e96be
Sahil Abrar <sahilabrar444@gmail.com>
2024-04-18 23:11:24 +0600

    added 7th img

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -53,7 +53,7 @@ const squareData = [
   },
   {
     id: 7,
-    src: 'https://images.unsplash.com/photo-1599586120429-48281b6f0ece?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+    src: 'https://i.ibb.co/Fz8FkjT/TEKKEN-SITE.webp',
   },
   {
     id: 8,
