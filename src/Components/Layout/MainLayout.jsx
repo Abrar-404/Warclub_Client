07abd77f391b22bec0a4d10f37369a3fb8f1aeee
Sahil Abrar <sahilabrar444@gmail.com>
2024-04-18 22:15:26 +0600

    added preloader code

--- src/Components/Layout/MainLayout.jsx
+++ src/Components/Layout/MainLayout.jsx
@@ -52,6 +52,7 @@ const MainLayout = () => {
         <div className="md:max-w-[768px] max-w-[428px] lg:max-w-[1400px] mx-auto ">
           <Navbar></Navbar>
           <Outlet></Outlet>
+          <Circle2></Circle2>
         </div>
       </div>
     </div>
