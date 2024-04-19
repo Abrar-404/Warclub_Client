97563e8c6a7f54228ce0c996eced0725a4e9b065
Sahil Abrar <sahilabrar444@gmail.com>
2024-04-19 22:33:04 +0600

    imported pic

--- src/Components/Tournaments/TournamentsFetch.jsx
+++ src/Components/Tournaments/TournamentsFetch.jsx
@@ -1,9 +1,11 @@
+import tourPic from '../../assets/tourPic.png';
+
 const TournamentsFetch = () => {
   return (
     <div>
-      <div className="mt-96">
-        <img className="border-2 border-red-700 w-32 h-32" src="../../assets/tourPic.png" alt="" />
-   </div>
+      <div className="mt-96 flex justify-center">
+        <img className="border-2 border-red-700 w-[50%]" src={tourPic} alt="" />
+      </div>
     </div>
   );
 };
