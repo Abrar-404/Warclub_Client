066adc8fd0e443a5677e7442adeaacc2b4e6e1d5
Sahil Abrar <sahilabrar444@gmail.com>
2024-04-19 22:36:02 +0600

    added about bg pic

--- src/Components/Tournaments/TournamentsFetch.jsx
+++ src/Components/Tournaments/TournamentsFetch.jsx
@@ -1,12 +1,31 @@
+import { Link } from 'react-router-dom';
 import tourPic from '../../assets/tourPic.png';
+import { IoIosArrowForward } from 'react-icons/io';
+import aboutBg from '../../assets/aboutusbg.png';
 
 const TournamentsFetch = () => {
   return (
-    <div>
-      <div className="flex justify-center mx-auto">
-        <img className=" w-[50%]" src={tourPic} alt="" />
+    <>
+      <div style={{ backgroundImage: `url(${aboutBg})` }}>
+        <div className="mx-auto pt-32 pb-32">
+          <h1 className="text-white font-bold lg:text-3xl text-center">
+            About Us
+          </h1>
+
+          <div className="text-white font-semibold text-sm text-center flex justify-center mx-auto items-center pt-2">
+            <h1>
+              <Link to="/">
+                <span className="text-green-400">Home</span>
+              </Link>{' '}
+            </h1>
+            <div>
+              <IoIosArrowForward></IoIosArrowForward>
+            </div>
+            <div>About Us</div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
