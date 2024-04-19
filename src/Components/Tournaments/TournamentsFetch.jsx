46098948a0dee8db7d2a0fb2d4a3294f3ad8e066
Sahil Abrar <sahilabrar444@gmail.com>
2024-04-19 23:14:48 +0600

    added flex gap

--- src/Components/Tournaments/TournamentsFetch.jsx
+++ src/Components/Tournaments/TournamentsFetch.jsx
@@ -52,19 +52,26 @@ const TournamentsFetch = () => {
             alt=""
           />
 
-          <div className="text-right absolute bottom-[80px] left-[40%]">
-            <div>
+          <div className="absolute bottom-[80px] left-[40%] flex justify-center gap-10">
+            <div className="text-right ">
               <h1 className="text-[#45F882] text-xl font-semibold">
                 VIDEO GAME
               </h1>
               <h1 className="text-3xl font-bold text-white">THE PANDAS</h1>
             </div>
 
-            <div className="text-center bg-black rounded-3xl">
+            <div className="text-center bg-black rounded-3xl p-3">
               <h1 className="text-white text-5xl font-semibold">06:30</h1>
               <h1 className="text-xs font-bold pt-3 text-[#959DB1]">
                 April 19, 2024
               </h1>
+            </div>
+
+            <div className="text-left">
+              <h1 className="text-[#45F882] text-xl font-semibold">
+                VIDEO GAME
+              </h1>
+              <h1 className="text-3xl font-bold text-white">THE PANDAS</h1>
             </div>
           </div>
         </div>
