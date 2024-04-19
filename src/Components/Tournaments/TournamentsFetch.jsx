562a839ca2e36931383182deb9908ffd03d4dfd7
Sahil Abrar <sahilabrar444@gmail.com>
2024-04-19 22:33:21 +0600

    added mx auto

--- src/Components/Tournaments/TournamentsFetch.jsx
+++ src/Components/Tournaments/TournamentsFetch.jsx
@@ -3,7 +3,7 @@ import tourPic from '../../assets/tourPic.png';
 const TournamentsFetch = () => {
   return (
     <div>
-      <div className="mt-96 flex justify-center">
+      <div className="mt-96 flex justify-center mx-auto">
         <img className="border-2 border-red-700 w-[50%]" src={tourPic} alt="" />
       </div>
     </div>
