a153a674aecbfca877fb2e8988adef798dbf069f
Sahil Abrar <sahilabrar444@gmail.com>
2024-04-22 20:01:49 +0600

    added lg right

--- src/Components/Tournaments/Tournaments.jsx
+++ src/Components/Tournaments/Tournaments.jsx
@@ -40,7 +40,7 @@ const Tournaments = () => {
                 alt=""
               />
               <img
-                className="absolute lg:bottom-[70px] lg:left-[7%] md:w-[13%] md:h-[80%] w-[12%] h-[80%] md:bottom-[10%] bottom-[10%] md:right-[4%] right-[5%]"
+                className="absolute lg:bottom-[70px] lg:right-[7%] md:w-[13%] md:h-[80%] w-[12%] h-[80%] md:bottom-[10%] bottom-[10%] md:right-[4%] right-[5%]"
                 src={sticker2}
                 alt=""
               />
@@ -57,234 +57,21 @@ const Tournaments = () => {
               </div>
 
               <div className="text-center bg-black rounded-3xl p-3">
-                <h1 className="text-white lg:text-5xl md:text-3xl font-semibold">06:30</h1>
+                <h1 className="text-white lg:text-5xl md:text-3xl text-xl font-semibold">
+                  06:30
+                </h1>
                 <h1 className="text-xs font-bold pt-3 text-[#959DB1]">
                   April 19, 2024
                 </h1>
               </div>
 
               <div className="text-left">
-                <h1 className="text-[#45F882] md:text-sm text-xl font-semibold">
+                <h1 className="text-[#45F882] lg:text-xl md:text-sm text-xs font-semibold">
                   VIDEO GAME
                 </h1>
-                <h1 className="text-3xl font-bold text-white">THE NINJAS</h1>
-              </div>
-            </div>
-
-            <div className="absolute left-[41%] bottom-[1%] flex justify-center items-center gap-4">
-              <div className="relative">
-                <img className="" src={upcoming} alt="" />
-                <h1 className="absolute bottom-[2px] font-bold left-4 text-white">
-                  Upcoming
+                <h1 className="lg:text-3xl font-bold md:text-xl text-lg">
+                  THE NINJAS
                 </h1>
-              </div>
-              <div className="relative ">
-                <img className="" src={upcoming} alt="" />
-                <h1 className="absolute bottom-[2px] font-bold left-9 text-white">
-                  0 / 0
-                </h1>
-              </div>
-            </div>
-          </div>
-
-          <div className="mt-20 relative">
-            <div>
-              <img className="" src={tourPic} alt="" />
-
-              <img
-                className="absolute bottom-[70px] left-[7%]"
-                src={sticker3}
-                alt=""
-              />
-              <img
-                className="absolute bottom-[70px] right-[7%]"
-                src={sticker4}
-                alt=""
-              />
-            </div>
-
-            <div className="absolute bottom-[80px] left-[26%] flex justify-center gap-10">
-              <div className="text-right ">
-                <h1 className="text-[#45F882] text-xl font-semibold">
-                  VIDEO GAME
-                </h1>
-                <h1 className="text-3xl font-bold text-white">THE HOLIGANS</h1>
-              </div>
-
-              <div className="text-center bg-black rounded-3xl p-3">
-                <h1 className="text-white text-5xl font-semibold">06:30</h1>
-                <h1 className="text-xs font-bold pt-3 text-[#959DB1]">
-                  April 18, 2024
-                </h1>
-              </div>
-
-              <div className="text-left">
-                <h1 className="text-[#45F882] text-xl font-semibold">
-                  VIDEO GAME
-                </h1>
-                <h1 className="text-3xl font-bold text-white">PRO PLAYERS</h1>
-              </div>
-            </div>
-
-            <div className="absolute left-[41%] bottom-[1%] flex justify-center items-center gap-4">
-              <div className="relative">
-                <img className="" src={upcoming} alt="" />
-                <h1 className="absolute bottom-[2px] font-bold left-4 text-white">
-                  Upcoming
-                </h1>
-              </div>
-              <div className="relative ">
-                <img className="" src={upcoming} alt="" />
-                <h1 className="absolute bottom-[2px] font-bold left-9 text-white">
-                  0 / 0
-                </h1>
-              </div>
-            </div>
-          </div>
-          <div className="mt-20 relative">
-            <div>
-              <img className="" src={tourPic} alt="" />
-
-              <img
-                className="absolute bottom-[70px] left-[7%]"
-                src={sticker5}
-                alt=""
-              />
-              <img
-                className="absolute bottom-[70px] right-[7%]"
-                src={sticker6}
-                alt=""
-              />
-            </div>
-
-            <div className="absolute bottom-[80px] left-[29%] flex justify-center gap-10">
-              <div className="text-right ">
-                <h1 className="text-[#45F882] text-xl font-semibold">
-                  VIDEO GAME
-                </h1>
-                <h1 className="text-3xl font-bold text-white">THE LIONS</h1>
-              </div>
-
-              <div className="text-center bg-black rounded-3xl p-3">
-                <h1 className="text-white text-5xl font-semibold">06:30</h1>
-                <h1 className="text-xs font-bold pt-3 text-[#959DB1]">
-                  April 19, 2024
-                </h1>
-              </div>
-
-              <div className="text-left">
-                <h1 className="text-[#45F882] text-xl font-semibold">
-                  VIDEO GAME
-                </h1>
-                <h1 className="text-3xl font-bold text-white">THE ASSASSINS</h1>
-              </div>
-            </div>
-
-            <div className="absolute left-[41%] bottom-[1%] flex justify-center items-center gap-4">
-              <div className="relative">
-                <img className="" src={upcoming} alt="" />
-                <h1 className="absolute bottom-[2px] font-bold left-4 text-white">
-                  Upcoming
-                </h1>
-              </div>
-              <div className="relative ">
-                <img className="" src={upcoming} alt="" />
-                <h1 className="absolute bottom-[2px] font-bold left-9 text-white">
-                  0 / 0
-                </h1>
-              </div>
-            </div>
-          </div>
-          <div className="mt-20 relative">
-            <div>
-              <img className="" src={tourPic} alt="" />
-
-              <img
-                className="absolute bottom-[70px] left-[7%]"
-                src={sticker1}
-                alt=""
-              />
-              <img
-                className="absolute bottom-[70px] right-[7%]"
-                src={sticker2}
-                alt=""
-              />
-            </div>
-
-            <div className="absolute bottom-[80px] left-[28%] flex justify-center gap-10">
-              <div className="text-right ">
-                <h1 className="text-[#45F882] text-xl font-semibold">
-                  VIDEO GAME
-                </h1>
-                <h1 className="text-3xl font-bold text-white">THE PANDAS</h1>
-              </div>
-
-              <div className="text-center bg-black rounded-3xl p-3">
-                <h1 className="text-white text-5xl font-semibold">06:30</h1>
-                <h1 className="text-xs font-bold pt-3 text-[#959DB1]">
-                  April 19, 2024
-                </h1>
-              </div>
-
-              <div className="text-left">
-                <h1 className="text-[#45F882] text-xl font-semibold">
-                  VIDEO GAME
-                </h1>
-                <h1 className="text-3xl font-bold text-white">THE NINJAS</h1>
-              </div>
-            </div>
-
-            <div className="absolute left-[41%] bottom-[1%] flex justify-center items-center gap-4">
-              <div className="relative">
-                <img className="" src={upcoming} alt="" />
-                <h1 className="absolute bottom-[2px] font-bold left-4 text-white">
-                  Upcoming
-                </h1>
-              </div>
-              <div className="relative ">
-                <img className="" src={upcoming} alt="" />
-                <h1 className="absolute bottom-[2px] font-bold left-9 text-white">
-                  0 / 0
-                </h1>
-              </div>
-            </div>
-          </div>
-          <div className="mt-20 relative">
-            <div>
-              <img className="" src={tourPic} alt="" />
-
-              <img
-                className="absolute bottom-[70px] left-[7%]"
-                src={sticker1}
-                alt=""
-              />
-              <img
-                className="absolute bottom-[70px] right-[7%]"
-                src={sticker2}
-                alt=""
-              />
-            </div>
-
-            <div className="absolute bottom-[80px] left-[28%] flex justify-center gap-10">
-              <div className="text-right ">
-                <h1 className="text-[#45F882] text-xl font-semibold">
-                  VIDEO GAME
-                </h1>
-                <h1 className="text-3xl font-bold text-white">THE PANDAS</h1>
-              </div>
-
-              <div className="text-center bg-black rounded-3xl p-3">
-                <h1 className="text-white text-5xl font-semibold">06:30</h1>
-                <h1 className="text-xs font-bold pt-3 text-[#959DB1]">
-                  April 19, 2024
-                </h1>
-              </div>
-
-              <div className="text-left">
-                <h1 className="text-[#45F882] text-xl font-semibold">
-                  VIDEO GAME
-                </h1>
-                <h1 className="text-3xl font-bold text-white">THE NINJAS</h1>
               </div>
             </div>
 
