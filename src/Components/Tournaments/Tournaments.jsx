37c9bcbf7250b14132fcd6400251894bd14c45eb
Sahil Abrar <sahilabrar444@gmail.com>
2024-04-22 20:12:17 +0600

    added left

--- src/Components/Tournaments/Tournaments.jsx
+++ src/Components/Tournaments/Tournaments.jsx
@@ -78,7 +78,7 @@ const Tournaments = () => {
             <div className="absolute left-[41%] bottom-[1%] flex justify-center items-center gap-4">
               <div className="relative">
                 <img className="md:w-[90%]" src={upcoming} alt="" />
-                <h1 className="absolute bottom-[2px] font-bold left-4 text-white">
+                <h1 className="absolute bottom-[2px] lg:font-bold md:font-semibold left-3 text-white">
                   Upcoming
                 </h1>
               </div>
