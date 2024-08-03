79e4653810b89b8c422dc6d09a39c11f54b82967
Al Fattaul Islam <139103740+Abrar-404@users.noreply.github.com>
2024-08-03 20:49:27 +0600

    added scrollbar style

--- src/Components/Layout/MainLayout.jsx
+++ src/Components/Layout/MainLayout.jsx
@@ -3,7 +3,6 @@ import Navbar from '../Navbar/Navbar';
 import '../Styles/cursor.css';
 import AnimatedCursor from 'react-animated-cursor';
 
-
 const MainLayout = () => {
   const background = {
     backgroundImage: `url(https://themedox.com/demo/mykd/assets/img/bg/area_bg02.jpg)`,
@@ -15,8 +14,6 @@ const MainLayout = () => {
   return (
     <div className="svg " style={background}>
       <div className="mx-auto">
-        {/* <SmoothScroll></SmoothScroll> */}
-
         <AnimatedCursor
           innerSize={10}
           outerSize={38}
@@ -32,22 +29,6 @@ const MainLayout = () => {
             border: '3px solid white',
           }}
         />
-
-        {/* <AnimatedCursor
-          innerSize={10}
-          outerSize={50}
-          innerScale={1}
-          outerScale={2}
-          outerAlpha={0}
-          trailingSpeed={4}
-          hasBlendMode={true}
-          innerStyle={{
-            backgroundColor: 'white',
-          }}
-          outerStyle={{
-            mixBlendMode: 'exclusion',
-          }}
-        /> */}
 
         <div className="md:max-w-[768px] max-w-[428px] lg:max-w-[1400px] mx-auto ">
           <Navbar></Navbar>
