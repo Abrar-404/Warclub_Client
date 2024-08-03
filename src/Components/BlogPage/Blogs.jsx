816dcda24e4e68b4bf3a4a9c99e9b63df24ad29c
Al Fattaul Islam <139103740+Abrar-404@users.noreply.github.com>
2024-08-03 20:19:54 +0600

    added blogs page banner

--- src/Components/BlogPage/Blogs.jsx
+++ src/Components/BlogPage/Blogs.jsx
@@ -1,6 +1,29 @@
+import { IoIosArrowForward } from 'react-icons/io';
+import aboutBg from '../../assets/aboutusbg.png';
+import { Link } from 'react-router-dom';
+
 const Blogs = () => {
   return (
     <div>
+      <div style={{ backgroundImage: `url(${aboutBg})` }}>
+        <div className="mx-auto pt-32 pb-32">
+          <h1 className="text-white font-bold lg:text-3xl text-center">
+            Latest News
+          </h1>
+
+          <div className="text-white font-semibold text-sm text-center flex justify-center mx-auto items-center pt-2">
+            <h1>
+              <Link to="/">
+                <span className="text-green-400">Home</span>
+              </Link>{' '}
+            </h1>
+            <div>
+              <IoIosArrowForward />
+            </div>
+            <div>Blogs</div>
+          </div>
+        </div>
+      </div>
       <h1>Blogs here</h1>
     </div>
   );
