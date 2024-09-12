af0a8ff9d66a9f22288e4d8695b47678048f6673
Al Fattaul Islam <139103740+Abrar-404@users.noreply.github.com>
2024-09-12 23:21:34 +0600

    added card styles

--- src/Components/BlogPage/BlogsCard.jsx
+++ src/Components/BlogPage/BlogsCard.jsx
@@ -5,16 +5,18 @@ const BlogsCard = ({ blog }) => {
 
   return (
     <>
-      <div class="blog_card">
-        <div class="blog_card-image"></div>
+      <div class="blog_card mb-8">
+        <div class="blog_card-image">
+          <img
+            src={image}
+            alt=""
+            className="w-full h-[370px] p-2 rounded-3xl"
+          />
+        </div>
         <p class="blog_card-title">{title}</p>
         <p class="blog_card-body">
           Nullam ac tristique nulla, at convallis quam. Integer consectetur mi
           nec magna tristique, non lobortis.
-        </p>
-        <p class="blog_footer">
-          Written by <span class="by-name">John Doe</span> on{' '}
-          <span class="date">25/05/23</span>
         </p>
       </div>
     </>
