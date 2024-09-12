c4d17e110e3ba7f61c6cdf89b19fd84ff81f94a9
Al Fattaul Islam <139103740+Abrar-404@users.noreply.github.com>
2024-09-12 23:08:40 +0600

    added input border color

--- src/Components/BlogPage/Blogs.jsx
+++ src/Components/BlogPage/Blogs.jsx
@@ -3,7 +3,7 @@ import aboutBg from '../../assets/aboutusbg.png';
 import { Link } from 'react-router-dom';
 import BlogsCard from './BlogsCard';
 import { useEffect, useState } from 'react';
-import '../Styles/blogCard.css'
+import '../Styles/blogCard.css';
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
@@ -45,9 +45,29 @@ const Blogs = () => {
 
         <div>
           <div class="blog_card">
-
-            <p class="blog_card-title">search</p>
-            
+            <div className="relative flex items-center">
+              <input
+                type="text"
+                placeholder="Search here..."
+                className="w-72 h-12 px-4 pl-8 pr-10 text-white bg-transparent border-2 border-[#45f882] rounded-full outline-none focus:ring-2 focus:ring-green-300 focus:border-[#45f882]"
+              />
+              <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                <svg
+                  className="w-6 h-6 text-green-400"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </div>
+            </div>
           </div>
         </div>
       </div>
