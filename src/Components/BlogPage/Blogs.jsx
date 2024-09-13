a3bcd9a56b6a2754e69d4e91167e991285dd8f09
Al Fattaul Islam <139103740+Abrar-404@users.noreply.github.com>
2024-09-13 21:34:02 +0600

    added category

--- src/Components/BlogPage/Blogs.jsx
+++ src/Components/BlogPage/Blogs.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import BlogsCard from './BlogsCard';
 import { useEffect, useState } from 'react';
 import '../Styles/blogCard.css';
+import '../Styles/categoryBtn.css';
 import { Search } from 'lucide-react';
 
 const Blogs = () => {
@@ -54,6 +55,33 @@ const Blogs = () => {
             <div className="absolute lg:right-10 lg:top-11 md:right-10 md:top-11 right-10 top-11">
               <Search color="#45f882" />
             </div>
+          </div>
+
+          <div class="blog_card">
+            <div>
+              <p className="text-white text-[28px] font-semibold">Categories</p>
+
+              <hr className="h-[2px] mt-2 mb-5 bg-gradient-to-r from-green-400 via-yellow-400 to-orange-500 border-0 rounded-full" />
+            </div>
+
+            <button className="cat_btn w-full">
+              <svg
+                className="cat_svg"
+                height="24"
+                width="24"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M0 0h24v24H0z" fill="none"></path>
+                <path
+                  d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+              <span className="cat_span flex justify-between mx-auto w-full">
+                <p>Fantasy</p> <p>(1)</p>
+              </span>
+            </button>
           </div>
         </div>
       </div>
